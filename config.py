6bd431235ee0a47257628059f79f4659da8c5817
murylo <muryloalves15@gmail.com>
2025-06-01 22:06:49 -0300

    arruma porta

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.config['HOST'] = '0.0.0.0'
-app.config['PORT']=8000
+app.config['PORT']=5000
 app.config['DEBUG'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
